fa0125b0088c8449a57df6fc7a3a0b8b011d0a97
Wiive <wirrechan@hotmail.com>
2020-10-14 15:58:11 +0200

    Uploaded Assignment12 with Extracurricular

---
+++ Programming Fundamentals/12InputAndMovement/Assets/Scripts/PlayerControllerClyde.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerClyde : MonoBehaviour
+{
+    //Uses velocity movement
+    public float speed;
+    Rigidbody2D rb2d;
+    Vector2 movement = new Vector2();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        movement = new Vector3(x, y);
+        if (movement.sqrMagnitude > 1)
+        {
+            movement = movement.normalized;
+        }   
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.velocity = movement * speed;
+    }
+}
---
+++ Programming Fundamentals/12InputAndMovement/Assets/Scripts/PlayerControllerShadow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerShadow : MonoBehaviour
+{
+    //Own (not better) movement that uses tranform up and right.
+    
+    public float speed;
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            transform.position += transform.up * -speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            transform.position += transform.right * -speed * Time.deltaTime;
+        }
+    }
+}
