bdff41fc534d441d9730c80c1d70ef5e7b5b8ac4
Wiive <wirrechan@hotmail.com>
2020-10-26 12:54:23 +0100

    Updated Assignment13 with Extra

---
+++ Programming Fundamentals/13Physics/Assets/Scripts/PlayerControllerGrinchen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerGrinchen : MonoBehaviour
+{
+    //Uses Translate movement
+    public float speed;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, y, 0) * speed * Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
---
+++ Programming Fundamentals/13Physics/Assets/Scripts/PlayerControllerPinky.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerPinky : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Movement
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(x, y);
+
+        rb2d.AddForce(movement * speed);
+    }
+}
